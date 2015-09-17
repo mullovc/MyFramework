e4e2769a08e1952aa7e4c32cf0bb462794d3993e
mullovc <mullovc@hotmail.de>
2015-09-17 18:17:57 +0200

    implemented decorator pattern for frames
    - added abstract decorator for GUI elements
    - added frame decorator
    - reoved old frame

---
+++ FrameDecorator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyMath;
+using MyFramework.GUI.GUIElements;
+
+namespace MyFramework.GUI.GUIElements
+{
+    class FrameDecorator : GUIDecorator
+    {
+        protected int frameWidth;
+        protected int frameHeight;
+
+        public enum FrameType
+        {
+            NONE,
+            PLAIN
+        }
+
+        public FrameDecorator(GUIElement element, FrameType type)
+            : base(element)
+        {
+            size = new Vector2(element.getWidth() + frameWidth * 2, element.getHeight() + frameHeight * 2);
+            graphic = new Image(size);
+            visible = true;
+
+            drawFrame();
+        }
+
+        private void loadFrame()
+        {
+            frameHeight = 2;
+            frameWidth = 2;
+        }
+
+        private void drawFrame()
+        {
+            for (int i = 1; i < size.x - 1; i++)
+            {
+                graphic.setPixel('_', new Vector2(i, 0));
+                graphic.setPixel('_', new Vector2(i, size.y - 1));
+            }
+
+            for (int i = 1; i < size.y; i++)
+            {
+                graphic.setPixel('|', new Vector2(0, i));
+                graphic.setPixel('|', new Vector2(size.x - 1, i));
+            }
+
+        }
+
+        public override Image getGraphic()
+        {
+            graphic.add(component.getGraphic(), new Vector2(frameWidth, frameHeight));
+            return base.getGraphic();
+        }
+    }
+
+}
---
+++ GUIDecorator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyMath;
+
+namespace MyFramework.GUI.GUIElements
+{
+    abstract class GUIDecorator : GUIElement
+    {
+        protected GUIElement component;
+
+        public GUIDecorator(GUIElement comp)
+        {
+            component = comp;
+        }
+
+        public GUIDecorator(GUIElement comp, Vector2 size)
+            : base(size)
+        {
+            component = comp;
+        }
+    }
+}
