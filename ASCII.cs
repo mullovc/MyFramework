241ccf0078e405a868f3af96f8d81b975cfb288a
mullovc <mullovc@hotmail.de>
2017-09-17 01:32:26 +0200

    changes and additions to ASCII class

--- ASCII.cs
+++ ASCII.cs
@@ -4,27 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using MyMath;
+using System.IO;
 
 namespace MyFramework.GUI
 {
     public class ASCII
     {
         static Image[]  ascii;
-        static Bitmap[] charSet;
         static int[]    charSetGrayValues;
-        static Font     font;
         static Font     textFont;
+        static float    fontResolution;
 
         const int firstPrintable = 32;
         const int setSize        = 127;
 
-        public static void GenerateASCIICharSet(Font f)
+        public static void GenerateASCIICharSet(Font font, bool useFile = true)
         {
-            font = f;
-
             int size = (int)font.Size;
 
-            charSet = new Bitmap[setSize - firstPrintable];
+            string fileName = "GUI\\ASCII\\" + font.Name + " " + font.Style.ToString();
+            if (useFile && File.Exists(fileName))
+            {
+                BinaryReader br = new BinaryReader(new FileStream(fileName, FileMode.Open));
+                byte[] gValBytes = br.ReadBytes(setSize - firstPrintable);
+                charSetGrayValues = new int[setSize - firstPrintable];
+                for (int i = 0; i < charSetGrayValues.Length; i++)
+                {
+                    charSetGrayValues[i] = (int)gValBytes[i];
+                }
+                return;
+            }
+
+            Bitmap[] charSet = new Bitmap[setSize - firstPrintable];
             for (int i = 0; i < charSet.Length; i++)
             {
                 charSet[i] = FontToBmp(font, (char) (i + firstPrintable));
@@ -34,13 +46,45 @@ namespace MyFramework.GUI
             charSetGrayValues = new int[charSet.Length];
             for (int i = 0; i < charSet.Length; i++)
             {
-                charSetGrayValues[i] = MeanGreyVal(charSet[i]);
+                charSetGrayValues[i] = (byte)MeanGreyVal(charSet[i]);
             }
+
+            charSetGrayValues = Normalize(charSetGrayValues, 32, 255);
+
+            FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate);
+            if (useFile && fs.CanWrite)
+            {
+                BinaryWriter bw = new BinaryWriter(fs);
+                byte[] gValBytes = new byte[charSetGrayValues.Length];
+                for (int i = 0; i < charSetGrayValues.Length; i++)
+                {
+                    gValBytes[i] = (byte)charSetGrayValues[i];
+                }
+                bw.Write(gValBytes);
+                bw.Close();
+            }
+            fs.Close();
+        }
+        
+        static int[] Normalize(int[] x, int min, int max)
+        {
+            float minVal = x.Min();
+            float maxVal = x.Max();
+            int[] normalized = new int[x.Length];
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                normalized[i] = (int) ((max - min) / (maxVal - minVal) * (x[i] - minVal)) + min;
+            }
+
+            return normalized;
         }
 
-        public static void SetTextFont(Font f)
+        public static void SetTextFont(Font f, float res = 8)
         {
+            fontResolution = res;
             textFont = f;
+            ascii = null;
         }
 
         public static Image charToImage(char c)
@@ -53,7 +97,11 @@ namespace MyFramework.GUI
 
             if (ascii[i] == null)
             {
-                ascii[i] = new Image(ImageToASCII(FontToBmp(textFont, c)));
+                if (textFont == null)
+                {
+                    SetTextFont(new Font(new FontFamily("Consolas"), 12), 16);
+                }
+                ascii[i] = new Image(ImageToASCII(FontToBmp(textFont, c), 1 / fontResolution));
             }
 
             return ascii[i];
@@ -101,23 +149,22 @@ namespace MyFramework.GUI
             return mean / (width * height);
         }
 
-        public static int[,] ApproxMeanGreyValWithCharSet(Bitmap image)
+        public static int[,] ApproxMeanGreyVal(Bitmap image, int xRes, int yRes)
         {
-            //assumes identical dimensions of all candidates
-            int candWidth  = charSet[0].Width;
-            int candHeight = charSet[0].Height;
-            int hPixls = image.Width  / candWidth;
-            int vPixls = image.Height / candHeight;
-            int[,] res = new int[hPixls, vPixls];
+            //int hPixls = image.Width  / kernelSize;
+            //int vPixls = image.Height / kernelSize;
+            int[,] res = new int[xRes, yRes];
+            int kernelX = image.Width  / xRes;
+            int kernelY = image.Height / yRes;
 
-            for (int i = 0; i < hPixls; i++)
+            for (int i = 0; i < xRes; i++)
             {
-                for (int j = 0; j < vPixls; j++)
+                for (int j = 0; j < yRes; j++)
                 {
-                    int cutoutGreyVal = MeanGreyVal(image, i * candWidth, j * candHeight, candWidth, candHeight);
+                    int cutoutGreyVal = MeanGreyVal(image, i * kernelX, j * kernelY, kernelX, kernelY);
                     int max = 0;
                     int argmax = 0;
-                    for (int k = 0; k < charSet.Length; k++)
+                    for (int k = 0; k < charSetGrayValues.Length; k++)
                     {
                         int curr = 255 - Math.Abs(charSetGrayValues[k] - cutoutGreyVal);
                         if (curr > max)
@@ -132,23 +179,40 @@ namespace MyFramework.GUI
             return res;
         }
 
-        public static char[,] ImageToASCII(Bitmap image)
+        public static char[,] ImageToASCII(Bitmap image, int resolutionX, int resolutionY)
         {
             if (charSetGrayValues == null)
             {
                 return null;
             }
 
-            int[,] matches = ApproxMeanGreyValWithCharSet(image);
+            int[,] matches = ApproxMeanGreyVal(image, resolutionX, resolutionY);
             char[,] art = new char[matches.GetLength(1), matches.GetLength(0)];
             for (int i = 0; i < matches.GetLength(0); i++)
             {
                 for (int j = 0; j < matches.GetLength(1); j++)
                 {
-                    art[j, i] = (char)(matches[i, j] + 32);
+                    art[j, i] = (char)(matches[i, j] + firstPrintable);
                 }
             }
             return art;
+        }
+
+        public static char[,] ImageToASCII(Bitmap image, float scale = 1)
+        {
+            return ImageToASCII(image, (int)(image.Width * scale), (int)(image.Height * scale));
+        }
+
+        public static Image FileToImage(string path, int resolutionX, int resolutionY)
+        {
+            Bitmap bmp = new Bitmap(path);
+            return new Image(ImageToASCII(bmp, resolutionX, resolutionY));
+        }
+
+        public static Image FileToImage(string path, float scale = 1)
+        {
+            Bitmap bmp = new Bitmap(path);
+            return new Image(ImageToASCII(bmp, (int)(bmp.Width * scale), (int)(bmp.Height * scale)));
         }
     }
 }
